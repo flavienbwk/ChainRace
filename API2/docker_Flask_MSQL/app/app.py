8d37c841a801da2a286875bc94487bea33a67f6e
GirardH <girardhugo77@gmail.com>
2019-06-27 19:14:52 +0200

    lets go, gotta do all the models hehe

--- API2/docker_Flask_MSQL/app/app.py
+++ API2/docker_Flask_MSQL/app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, make_response, jsonify
 from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
@@ -9,10 +10,24 @@ import datetime
 app = Flask(__name__)
 
 # configure db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chainrace.db'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = ''
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(128), unique=True, nullable=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=func.now)
+
+    def __repr__(self):
+        return f"User('{self.public_id}', '{self.username}', '{self.email}', '{self.created_at}')"
 
 mysql = MySQL(app)
 
@@ -20,9 +35,9 @@ mysql = MySQL(app)
 app.config['SECRET_KEY'] = 'thatsasecret'
 
 def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = request.args.get('token') #unvalid way
+    # @wraps(f)
+    def wrapper(*args, **kwargs):
+        token = request.headers.get('JWT-token')
         if not token:
             return jsonify({'message' : 'Token is missing'}), 403
         try:
@@ -31,14 +46,15 @@ def token_required(f):
             return jsonify({'message' : 'Token is invalid'}), 403
 
         return f(*args, **kwargs)
-    return decorated
+    return wrapper
+
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-
+# register senseY etre fonctionnel
 @app.route('/register', methods=['POST'])
 def register():
     public_id = str(uuid.uuid4())
@@ -51,32 +67,63 @@ def register():
     cur.close()
     return 'registered succesfuly'
 
-
+# login senseY etre fonctionnel
 @app.route('/login', methods=['POST'])
 def login():
     auth = request.form
-
-    if auth and auth.get('username') == 'xxkingxx' and auth.get('password') == '123456':
+    cur = mysql.connection.cursor()
+    user = cur.execute("SELECT * FROM users where username=" + auth.get('username'))
+    mysql.connection.commit()
+    cur.close()
+    if user is None:
+        return jsonify({'message' : 'invalid username'})
+    else if not check_password_hash(user.get('hash_password'), auth.get('password'))
+        return jsonify({'message' : 'invalid password'})
+    else 
         token = jwt.encode({'user' : auth.get('username'), 'exp' : datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, app.config['SECRET_KEY'])
         return jsonify({'token' : token.decode('UTF-8')})
+    return jsonify({'message' : 'didnt go in any loop. gotta investigate that quickly'})
 
-    print(auth.get('username'))
-    print(auth.get('password'))
-
-    return make_response('could not verify', 401, {'WWW-Authenticate' : 'Basic-realm:"Login Required"'})
-
-
-@app.route('/users')
-def users():
+# route senseY etre fonctionnelle
+@app.route('/users/get/all')
+@token_required
+def get_all_users():
     cur = mysql.connection.cursor()
     resValue = cur.execute("SELECT * FROM users")
     if resValue > 0:
         userDetails = cur.fetchall()
+    cur.close()
     return userDetails
 
+# route senseY etre fonctionnelle
+@app.route('/users/get/<public_id>')
+@token_required
+def get_one_user(public_id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users where ids=" + public_id)
+    user = cur.fetchone()
+    cur.close()
+    if not user:
+        return jsonify('message': 'user not found')
+    return jsonify('user': user)
 
-@app.route('/<userid>/addVehicule', methods=['POST'])
-def addVehicule(userid):
+# route senseY etre fonctionnelle
+@app.route('/users/delete/<public_id>', methods=['DELETE'])
+@token_required
+def delete_one_user(public_id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users where ids=" + public_id)
+    user = cur.fetchone()
+    if not user:
+        return jsonify('message': 'user not found')
+    cur.execute("DELETE FROM users where ids=" + public_id)
+    mysql.connection.commit()
+    cur.close()
+    return jsonify('message': 'user deleted')
+
+
+@app.route('/vehicule/claim/<userid>', methods=['POST'])
+def claim_vehicule(userid):
     return ''
 
 
